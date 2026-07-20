fd2b9a97ff8e67c32c8089fb8dfacd3a140e6a1e
CHANDANAcs@02 <chandanacs2004@gmail.com>
2026-07-20 08:43:35 +0530

    [Chandana] : Added NameSearch

--- src/NameSearch.java
+++ src/NameSearch.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+// Search contact by name
 
-public class NameSearch {
+public class NameSearch implements SearchCriteria {
+
+    private String name;
+
+    public NameSearch(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean matches(Contact contact) {
+
+        return contact.getName().equalsIgnoreCase(name);
+    }
 }
