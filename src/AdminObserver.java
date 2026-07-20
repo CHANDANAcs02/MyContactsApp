da9319dbf8e3432202414795a924fb69079075b7
CHANDANAcs@02 <chandanacs2004@gmail.com>
2026-07-20 08:37:35 +0530

    [Chandana] : Added AdminObserver

--- src/AdminObserver.java
+++ src/AdminObserver.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+// Admin gets notified when a contact is deleted
 
-public class AdminObserver {
+public class AdminObserver implements ContactObserver {
+
+    @Override
+    public void contactDeleted(Contact contact) {
+
+        System.out.println("Admin Notification : " +
+                contact.getName() + " was deleted.");
+    }
 }
