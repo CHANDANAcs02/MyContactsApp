08d93556ca5e3f3ce740c3dc00ee3352f918e811
CHANDANAcs@02 <chandanacs2004@gmail.com>
2026-07-20 08:49:10 +0530

    [Chandana] : Completed UC11 Create and Manage Tags

--- src/UseCase11ManageTagsApp.java
+++ src/UseCase11ManageTagsApp.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+// UC11 - Create and Manage Tags
 
 public class UseCase11ManageTagsApp {
+
+    public static void main(String[] args) {
+
+        Contact contact = new Person("John");
+
+        contact.addPhoneNumber(new PhoneNumber("0771234567"));
+        contact.addEmail(new Email("john@gmail.com"));
+
+        contact.addTag(TagFactory.getTag("Family"));
+        contact.addTag(TagFactory.getTag("Work"));
+        contact.addTag(TagFactory.getTag("Family"));
+
+        System.out.println(contact);
+    }
 }
