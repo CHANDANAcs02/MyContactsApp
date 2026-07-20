f3f76fd994d94798acfd7088c09928de6c8d8fed
CHANDANAcs@02 <chandanacs2004@gmail.com>
2026-07-20 08:28:11 +0530

    [Chandana] : Added ContactFactory

--- src/ContactFactory.java
+++ src/ContactFactory.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class ContactFactory {
+
+    public static Contact createContact(String type, String name) {
+
+        if (type.equalsIgnoreCase("Organization")) {
+            return new Organization(name);
+        }
+
+        return new Person(name);
+    }
 }
