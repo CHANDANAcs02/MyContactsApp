ac0a20337d8bcd30d2ae2309cffe23f88ff5249f
CHANDANAcs@02 <chandanacs2004@gmail.com>
2026-07-20 08:46:13 +0530

    [Chandana] : Added NameSortStrategy

--- src/NameSortStrategy.java
+++ src/NameSortStrategy.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+// Sort contacts by name
 
-public class NameSortStrategy {
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class NameSortStrategy implements FilterStrategy {
+
+    @Override
+    public List<Contact> filter(List<Contact> contacts) {
+
+        Collections.sort(contacts, Comparator.comparing(Contact::getName));
+
+        return contacts;
+    }
 }
