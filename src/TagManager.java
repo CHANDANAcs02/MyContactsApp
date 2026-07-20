bf892fe1baae8b8de77710f6d628f9f6f142a878
CHANDANAcs@02 <chandanacs2004@gmail.com>
2026-07-20 08:51:18 +0530

    [Chandana] : Added TagManager

--- src/TagManager.java
+++ src/TagManager.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+// Manage tags
 
 public class TagManager {
+
+    private TagObserver observer;
+
+    public void setObserver(TagObserver observer) {
+        this.observer = observer;
+    }
+
+    public void addTag(Contact contact, Tag tag) {
+
+        contact.addTag(tag);
+
+        if (observer != null) {
+            observer.tagAdded(contact, tag);
+        }
+    }
 }
