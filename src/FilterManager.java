ec1cd7071b7c5be02477fa50f8f1ceec53b94daf
CHANDANAcs@02 <chandanacs2004@gmail.com>
2026-07-20 08:46:14 +0530

    [Chandana] : Added FilterManager

--- src/FilterManager.java
+++ src/FilterManager.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+// Executes selected filter strategy
+
+import java.util.List;
 
 public class FilterManager {
+
+    private FilterStrategy strategy;
+
+    public void setStrategy(FilterStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public List<Contact> execute(List<Contact> contacts) {
+        return strategy.filter(contacts);
+    }
 }
