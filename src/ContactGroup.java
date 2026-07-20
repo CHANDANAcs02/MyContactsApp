bd45946d5ecf2462b3d8986d973feea1dde13562
CHANDANAcs@02 <chandanacs2004@gmail.com>
2026-07-20 08:40:39 +0530

    [Chandana] : Added ContactGroup

--- src/ContactGroup.java
+++ src/ContactGroup.java
@@ -1,4 +1,41 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ContactGroup {
+// Group of contacts
+
+public class ContactGroup implements ContactComponent {
+
+    private String groupName;
+    private List<ContactComponent> contacts;
+
+    public ContactGroup(String groupName) {
+
+        this.groupName = groupName;
+        contacts = new ArrayList<>();
+    }
+
+    public void add(ContactComponent contact) {
+        contacts.add(contact);
+    }
+
+    public void remove(ContactComponent contact) {
+        contacts.remove(contact);
+    }
+
+    @Override
+    public void showDetails() {
+
+        System.out.println("\nGroup : " + groupName);
+
+        for (ContactComponent contact : contacts) {
+            contact.showDetails();
+        }
+    }
+
+    public void deleteAll() {
+
+        contacts.clear();
+
+        System.out.println("\nAll contacts removed from " + groupName);
+    }
 }
