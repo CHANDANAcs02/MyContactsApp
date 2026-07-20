748de4f7f58e0f69711e97c4f7906c85111820b1
CHANDANAcs@02 <chandanacs2004@gmail.com>
2026-07-20 08:43:35 +0530

    [Chandana] : Added EmailSearch

--- src/EmailSearch.java
+++ src/EmailSearch.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+// Search contact by email
 
-public class EmailSearch {
+public class EmailSearch implements SearchCriteria {
+
+    private String email;
+
+    public EmailSearch(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean matches(Contact contact) {
+
+        for (Email item : contact.getEmails()) {
+
+            if (item.getEmail().equalsIgnoreCase(email)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
