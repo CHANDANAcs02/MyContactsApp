7927c5a5a6d9cf29664e568a11260b4d5816f900
CHANDANAcs@02 <chandanacs2004@gmail.com>
2026-07-20 08:34:46 +0530

    [Chandana] : Completed UC6 Edit Contact

--- src/UseCase6EditContactApp.java
+++ src/UseCase6EditContactApp.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+// UC6 - Edit Contact
 
 public class UseCase6EditContactApp {
+
+    public static void main(String[] args) {
+
+        Contact contact = new Person("John");
+
+        contact.addPhoneNumber(new PhoneNumber("0771234567"));
+        contact.addEmail(new Email("john@gmail.com"));
+
+        System.out.println("Before Update");
+        System.out.println(contact);
+
+        UpdateContactCommand command =
+                new UpdateContactCommand(contact, "John Silva");
+
+        command.execute();
+
+        System.out.println("\nAfter Update");
+        System.out.println(contact);
+
+        command.undo();
+
+        System.out.println("\nAfter Undo");
+        System.out.println(contact);
+    }
 }
