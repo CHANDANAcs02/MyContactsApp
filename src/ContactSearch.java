e351d0825c0f43b7f87e8b2ab7c8730b26982e63
CHANDANAcs@02 <chandanacs2004@gmail.com>
2026-07-20 08:43:35 +0530

    [Chandana] : Added ContactSearch

--- src/ContactSearch.java
+++ src/ContactSearch.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.List;
+
+// Search contacts
 
 public class ContactSearch {
+
+    public static void search(List<Contact> contacts,
+                              SearchCriteria criteria) {
+
+        boolean found = false;
+
+        for (Contact contact : contacts) {
+
+            if (criteria.matches(contact)) {
+
+                System.out.println(contact);
+                found = true;
+            }
+        }
+
+        if (!found) {
+            System.out.println("No matching contact found.");
+        }
+    }
 }
