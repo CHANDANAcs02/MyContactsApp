385a7849929d9ab69921ad1c3bc66a5a0548459c
CHANDANAcs@02 <chandanacs2004@gmail.com>
2026-07-20 08:32:00 +0530

    [Chandana] : Added MaskEmailDecorator

--- src/MaskEmailDecorator.java
+++ src/MaskEmailDecorator.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+// Hide part of the email address
+public class MaskEmailDecorator extends ContactDecorator {
 
-public class MaskEmailDecorator {
+    public MaskEmailDecorator(Contact contact) {
+        super(contact);
+    }
+
+    @Override
+    public String toString() {
+
+        String email = contact.getEmails().get(0).getEmail();
+
+        int index = email.indexOf("@");
+
+        String masked = email.substring(0, 2)
+                + "****"
+                + email.substring(index);
+
+        return contact.toString().replace(email, masked);
+    }
 }
