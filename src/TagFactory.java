6dc445f1ee5edf8c33a0725f690149a965cf2979
CHANDANAcs@02 <chandanacs2004@gmail.com>
2026-07-20 08:49:10 +0530

    [Chandana] : Added TagFactory

--- src/TagFactory.java
+++ src/TagFactory.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+// Flyweight pattern
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class TagFactory {
+
+    private static Map<String, Tag> tags = new HashMap<>();
+
+    public static Tag getTag(String name) {
+
+        String key = name.toLowerCase();
+
+        if (!tags.containsKey(key)) {
+            tags.put(key, new Tag(name));
+        }
+
+        return tags.get(key);
+    }
 }
