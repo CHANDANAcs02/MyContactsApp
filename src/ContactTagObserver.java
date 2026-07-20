0e2cef036cbb965efcd2d43d513567629f8b0335
CHANDANAcs@02 <chandanacs2004@gmail.com>
2026-07-20 08:51:18 +0530

    [Chandana] : Added ContactTagObserver

--- src/ContactTagObserver.java
+++ src/ContactTagObserver.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+// Observer implementation
 
-public class ContactTagObserver {
+public class ContactTagObserver implements TagObserver {
+
+    @Override
+    public void tagAdded(Contact contact, Tag tag) {
+
+        System.out.println("Tag '" + tag.getName()
+                + "' added to " + contact.getName());
+    }
 }
