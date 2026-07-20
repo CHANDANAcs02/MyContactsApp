088973eab6788b14025ca0f82007975c89dbb71a
CHANDANAcs@02 <chandanacs2004@gmail.com>
2026-07-20 08:32:01 +0530

    [Chandana] : Completed UC5 View Contact Details

--- src/UseCase5ViewContactApp.java
+++ src/UseCase5ViewContactApp.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+// UC5 - View Contact Details
 
 public class UseCase5ViewContactApp {
+
+    public static void main(String[] args) {
+
+        Contact contact = new Person("John Smith");
+
+        contact.addPhoneNumber(new PhoneNumber("0771234567"));
+        contact.addEmail(new Email("johnsmith@gmail.com"));
+
+        System.out.println("Original Contact");
+        System.out.println(contact);
+
+        System.out.println("\nName in Uppercase");
+        Contact upper = new UpperCaseDecorator(contact);
+        System.out.println(upper);
+
+        System.out.println("\nMasked Email");
+        Contact masked = new MaskEmailDecorator(contact);
+        System.out.println(masked);
+    }
 }
