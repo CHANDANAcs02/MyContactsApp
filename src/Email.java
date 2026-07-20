d3098ac95772868d57fbc09f3d88b207e8fa3b6d
CHANDANAcs@02 <chandanacs2004@gmail.com>
2026-07-20 08:28:11 +0530

    [Chandana] : Added Email class

--- src/Email.java
+++ src/Email.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Email {
+
+    private String email;
+
+    public Email(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return email;
+    }
 }
