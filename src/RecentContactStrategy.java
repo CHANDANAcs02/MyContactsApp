6b27821bef41c4c54e005d5e478cfb5d3744e54b
CHANDANAcs@02 <chandanacs2004@gmail.com>
2026-07-20 08:46:14 +0530

    [Chandana] : Added RecentContactStrategy

--- src/RecentContactStrategy.java
+++ src/RecentContactStrategy.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+// Show recently added contacts
 
-public class RecentContactStrategy {
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class RecentContactStrategy implements FilterStrategy {
+
+    @Override
+    public List<Contact> filter(List<Contact> contacts) {
+
+        Collections.sort(contacts,
+                Comparator.comparing(Contact::getCreatedDate).reversed());
+
+        return contacts;
+    }
 }
