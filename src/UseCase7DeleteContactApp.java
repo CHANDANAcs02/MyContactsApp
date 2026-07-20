8dc1cdaa8c48acabf1a61784c3eceea368d7f1ef
CHANDANAcs@02 <chandanacs2004@gmail.com>
2026-07-20 08:37:36 +0530

    [Chandana] : Completed UC7 Delete Contact

--- src/UseCase7DeleteContactApp.java
+++ src/UseCase7DeleteContactApp.java
@@ -1,4 +1,32 @@
-package PACKAGE_NAME;
+// UC7 - Delete Contact
 
 public class UseCase7DeleteContactApp {
+
+    public static void main(String[] args) {
+
+        ContactManager manager = new ContactManager();
+
+        manager.setObserver(new AdminObserver());
+
+        Contact contact = new Person("John");
+
+        contact.addPhoneNumber(new PhoneNumber("0771234567"));
+        contact.addEmail(new Email("john@gmail.com"));
+
+        manager.addContact(contact);
+
+        System.out.println("Available Contacts");
+        manager.showContacts();
+
+        System.out.println("\nSoft Delete...");
+        manager.softDelete(contact);
+
+        System.out.println("\nAvailable Contacts");
+        manager.showContacts();
+
+        System.out.println("\nHard Delete...");
+        manager.hardDelete(contact);
+
+        System.out.println("\nCompleted.");
+    }
 }
